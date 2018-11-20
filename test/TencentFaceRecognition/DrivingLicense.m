a865c14004299f14e83e8bb57756fb1462381317
ZT <admin@AdmindeiMac-3.local>
2018-11-20 18:20:16 +0800

    1、添加 UI

--- test/TencentFaceRecognition/DrivingLicense.m
+++ test/TencentFaceRecognition/DrivingLicense.m
@@ -8,7 +8,8 @@
 
 #import "DrivingLicense.h"
 
-@interface DrivingLicense ()
+@interface DrivingLicense ()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *license;
 
 @end
 
@@ -17,6 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"驾驶证识别";
+}
+- (IBAction)openAlbum:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate = self; // 设置代理
+    picker.allowsEditing = NO;
+    [self.navigationController presentViewController:picker animated:YES completion:nil];
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    if ([mediaType isEqualToString:(NSString *)kUTTypeImage]) {//如果是拍照
+        //获取照片的原图
+        UIImage *original = [info objectForKey:UIImagePickerControllerOriginalImage];
+        [self.license setImage:original];
+//        [self imageToBase64Str];
+    }
+    // 销毁控制器
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"关闭");
+}
+
+- (IBAction)check:(id)sender {
 }
 
 @end
--- test/TencentFaceRecognition/TencentRecognitionViewController.m
+++ test/TencentFaceRecognition/TencentRecognitionViewController.m
@@ -8,12 +8,12 @@
 
 #import "TencentRecognitionViewController.h"
 #import "TencentIDCard.h"
+#import "DrivingLicense.h"
 
 @interface TencentRecognitionViewController ()<UITableViewDelegate,UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UITableView *tencentTable;
 
 @property (nonatomic, strong) NSMutableDictionary *dataSource;
-@property (nonatomic, strong) NSArray *subVC;
 @end
 
 @implementation TencentRecognitionViewController
@@ -22,8 +22,7 @@
     [super viewDidLoad];
     self.title = @"身份验证";
     
-    self.tencentTable.tableFooterView = [[UIView alloc]init];
-    self.tencentTable.sectionHeaderHeight = 40;
+    self.tencentTable.sectionFooterHeight = 0;
 }
 
 #pragma mark - table view data source
@@ -37,10 +36,16 @@
     NSArray *values = [self.dataSource objectForKey:key];
     return values.count;
 }
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 40.0;
+}
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
     UILabel *view = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 40)];
     view.text = [self.dataSource.allKeys objectAtIndex:section];
+    
+    NSLog(@"section header :%@",NSStringFromCGRect(view.frame));
     return view;
 }
 static NSString *cellIdentifier = @"TableCellIdentifier";
@@ -62,19 +67,43 @@ static NSString *cellIdentifier = @"TableCellIdentifier";
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (indexPath.section) {
-        switch (indexPath.row) {
-            case 0:{
-                TencentIDCard *idVC = [[TencentIDCard alloc]init];
-                [self.navigationController pushViewController:idVC animated:YES];
-            }
-                break;
-            case 1:{
-                TencentIDCard *idVC = [[TencentIDCard alloc]init];
-                [self.navigationController pushViewController:idVC animated:YES];
-            }
-                break;
+    NSInteger index = indexPath.section*10 + indexPath.row;
+    switch (index) {
+        case 0:{
+            TencentIDCard *idVC = [[TencentIDCard alloc]init];
+            [self.navigationController pushViewController:idVC animated:YES];
         }
+            break;
+        case 1:{
+            DrivingLicense *idVC = [[DrivingLicense alloc]init];
+            [self.navigationController pushViewController:idVC animated:YES];
+        }
+            break;
+        case 10:{
+            DrivingLicense *idVC = [[DrivingLicense alloc]init];
+            [self.navigationController pushViewController:idVC animated:YES];
+        }
+            break;
+        case 11:{
+            DrivingLicense *idVC = [[DrivingLicense alloc]init];
+            [self.navigationController pushViewController:idVC animated:YES];
+        }
+            break;
+        case 12:{
+            DrivingLicense *idVC = [[DrivingLicense alloc]init];
+            [self.navigationController pushViewController:idVC animated:YES];
+        }
+            break;
+        case 13:{
+            DrivingLicense *idVC = [[DrivingLicense alloc]init];
+            [self.navigationController pushViewController:idVC animated:YES];
+        }
+            break;
+        case 14:{
+            DrivingLicense *idVC = [[DrivingLicense alloc]init];
+            [self.navigationController pushViewController:idVC animated:YES];
+        }
+            break;
     }
 }
 
@@ -83,22 +112,14 @@ static NSString *cellIdentifier = @"TableCellIdentifier";
 {
     if (_dataSource == nil) {
         _dataSource = [[NSMutableDictionary alloc]init];
+        [_dataSource setObject:@[@"身份证识别",@"驾驶证识别"] forKey:@"对照片识别"];
         [_dataSource setObject:@[@"用户上传照片身份信息核验",
                                  @"活体检测—获取唇语验证码",
                                  @"活体检测视频身份信息核验",
                                  @"活体检测视频与用户照片的对比",
                                  @"人脸静态活体检测"] forKey:@"人脸核身"];
-        [_dataSource setObject:@[@"身份证识别",@"驾驶证识别"] forKey:@"照片识别"];
     }
     return _dataSource;
-}
-
-- (NSArray *)subVC
-{
-    if (_subVC == nil) {
-        _subVC = [NSArray arrayWithObjects:@[@"1-0",@"1-1"],@[@"0-0",@"0-1",@"0-2",@"0-3",@"0-4"],nil];
-    }
-    return _subVC;
 }
 
 @end
