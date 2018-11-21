200c677828e3d05465ceca7997d4328670649c7c
ZT <admin@AdmindeiMac-3.local>
2018-11-21 18:27:20 +0800

    1、修改人脸识别部分 UI；

---
+++ test/TencentFaceRecognition/Face/FaceIdCard.h
@@ -0,0 +1,17 @@
+//
+//  FaceIdCard.h
+//  test
+//
+//  Created by Admin on 2018/11/21.
+//  Copyright © 2018 Tima. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FaceIdCard : RootViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/TencentFaceRecognition/Face/VideoAndLipLanguage.h
@@ -0,0 +1,19 @@
+//
+//  VideoAndLipLanguage.h
+//  test
+//
+//  Created by Admin on 2018/11/21.
+//  Copyright © 2018 Tima. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VideoAndLipLanguage : RootViewController
+
+@property (nonatomic, strong) NSString *lipLanguage;
+
+@end
+
+NS_ASSUME_NONNULL_END
